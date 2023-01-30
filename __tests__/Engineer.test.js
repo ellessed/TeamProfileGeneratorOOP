1e98b2acd5ab21c22c350c9391fd218e0498654f
elleserdonaldson <ellessedon@gmail.com>
2023-01-30 00:08:43 +0000

    created engineer tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,32 @@
+const Engineer = require("../src/lib/Employee");
+
+const testEngineer = new Engineer(
+  "testEngineer",
+  12,
+  "testengineer@email.com",
+  "engineer"
+);
+
+describe("Engineer", () => {
+  it("Contains name", () => {
+    expect(testEngineer.name).toEqual(expect.any(String));
+  });
+
+  it("Is the name at least 2 chars long?", () => {
+    expect(testEngineer.name.length).toBeGreaterThanOrEqual(2);
+  });
+
+  it("Has an id?", () => {
+    expect(testEngineer.id).toEqual(expect.any(Number));
+  });
+
+  it("Contains an email", () => {
+    expect(testEngineer.email).toContainEqual(
+      expect.stringContaining("@", ".")
+    );
+  });
+
+  it("Contains a role", () => {
+    expect(testEngineer.role).toMatch("em");
+  });
+});
